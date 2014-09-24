9aa226bdb4409c9533cad22a2fa87a4c85c4b636
David Markus <11dmarkus@gmail.com>
2014-09-23 20:40:34 -0400

    Made some directories, added install script and nginx config for php.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ grocery_tracker
 ===============
 
 Track zee groceries. Make recommendations. Use SQL. Done.
+
+Directory Structure:
+--------------------
+* scripts: utility scripts and useful things.
+* static: static files, boring templates.
+* www: frontend files that should go in wwwroot
---
+++ scripts/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Script installs contents of www into nginx default web folder /usr/share/nginx/html/grocery_list
+# Could add case for apache...
+
+# Assume www folder in ./
+LOCAL_PATH='./www/'
+
+# Build WWW_USEDIR
+WWW_PREFIX='/usr/share/nginx/html/'
+WWW_SUBDIR='grocery_list/'
+WWW_USEDIR="${WWW_PREFIX}${WWW_SUBDIR}"
+
+# Create WWW_USDIR if not exists.
+if [ ! -d $WWW_USEDIR ]; then
+  printf 'Creating directory: %s\n' $WWW_USEDIR;
+  sudo mkdir -p $WWW_USEDIR;
+fi
+
+# Copy and install the things.
+printf 'Copying files from %s to %s...\n' $LOCAL_PATH $WWW_USEDIR
+sudo cp -r $LOCAL_PATH* $WWW_USEDIR
+
+if [ $? != 0 ]; then
+  printf 'Error copying files.\n'
+else
+  printf 'Done.\n'
+fi

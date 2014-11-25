3197e03fc51a58627f89e96bb33f733b9a32c711
David Markus <11dmarkus@gmail.com>
2014-11-24 22:50:40 -0500

    Added SQL initialization script for simplest possible use case (no users yet).

---
+++ scripts/initdb.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS  grocery_tracker;
+CREATE DATABASE grocery_tracker;
+USE grocery_tracker;
+
+/*
+CREATE TABLE Users(
+  user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) UNIQUE,
+  keyword  VARCHAR(255)
+);
+*/
+
+CREATE TABLE Items(
+  upc VARCHAR(12) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Lists(
+  /*
+  user_id INTEGER,
+  */
+  time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  /*
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+  PRIMARY KEY (user_id, time)
+  */
+  PRIMARY KEY (time)
+);
+
+CREATE TABLE ListItems(
+  /*
+  user_id INTEGER,
+  */
+  time TIMESTAMP,
+  upc VARCHAR(12),
+  /*
+  FOREIGN KEY (user_id, time) REFERENCES Lists(user_id, time),
+  */
+  FOREIGN KEY (time) REFERENCES Lists(time),
+  FOREIGN KEY (upc) REFERENCES Items(upc)
+);

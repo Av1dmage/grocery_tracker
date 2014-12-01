604c7f9b33618b09ebb02ed3c367083c2196eb18
David Markus <11dmarkus@gmail.com>
2014-12-01 00:05:25 -0500

    Added input warnings for UPC codes.

--- www/index.php
+++ www/index.php
@@ -20,12 +20,25 @@
       input.setAttribute("type", "text");
       input.setAttribute("name", "item" + String(window.itemCount));
       input.setAttribute("id"  , "item" + String(window.itemCount));
+      input.setAttribute("onblur" , "validateUPC('" + "item" + String(window.itemCount) + "')");
 
       // Append new li to list
       li.appendChild(input);
       ul.appendChild(li);
 
       window.itemCount = window.itemCount + 1;
+    }
+
+    function validateUPC(id) {
+      var reupc = /^[0-9]{12}$/
+      var input = document.getElementById(id);
+
+      if(!input.value.match(reupc)) {
+        alert("Error: UPCs must be 12 characters in length, and contain only numbers. e.g. '123123123123'.");
+        input.setAttribute("style" , "color: red;");
+      } else {
+        input.setAttribute("style" , "");
+      }
     }
 
     window.onload = onLoad;

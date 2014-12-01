780b488a3f612d5b004d159218b6fef9bde669b1
David Markus <11dmarkus@gmail.com>
2014-12-01 05:53:20 -0500

    Added viewlists.php. Changed a bit of CSS for unordered lists.

---
+++ www/viewlists.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+  <title>Grocery Tracker</title>
+
+  <?php include 'static/includes.php';?>
+</head>
+
+<html>
+
+  <?php
+    include 'static/navbar.php';
+  ?>
+
+  <!-- PUT STUFF HERE!!! -->  
+
+  <?php
+    include 'static/footer.php';
+  ?>
+
+</html>
